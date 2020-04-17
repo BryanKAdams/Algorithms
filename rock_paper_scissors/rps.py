eb62d9554487476354b81cd6988752219c20f98b
Bryan <17adamsbryk@washk12.org>
2020-04-17 11:57:31 -0600

    Finished a couple more

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,10 +1,25 @@
-#!/usr/bin/python
+#!/c/Users/Bryan/AppData/Local/Programs/Python/Python38/python
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
 
+def rock_paper_scissors(n):
+    base = [['rock'], ['paper'], ['scissors']]
+    possibleResults = [0] * 3 ** n
+    print(possibleResults)
+    if n == 0:
+        return [[]]
+    if n == 1:
+        return base
+    recursion = rock_paper_scissors(n-1)
+    i = 0
+    for item in recursion:
+        for action in base:
+            possibleResults[i] = item + action
+            i += 1
+    return possibleResults
+
+print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
